ae43dc7d77e5212e31fc2b784423d1b28564d1b7
Kyrylo Fursov <kirfurs31@gmail.com>
2025-05-02 21:09:03 +0300

    Refactor order insertion

--- task.sql
+++ task.sql
@@ -6,18 +6,16 @@ VALUES ('AwersomeProduct', 'Product Desctiption', 5, 42);
 INSERT INTO Customers (FirstName, LastName, Email, Address)
 VALUES ('John', 'Dou', 'j@dou.ua', 'far, far away');
 
-START TRANSACTION; 
-
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 
-SET @order_id = LAST_INSERT_ID();
+START TRANSACTION;
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (@order_id, 1, 1);
+VALUES (1, 1, 1);
 
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
 WHERE ID = 1;
 
-COMMIT; 
+COMMIT;
