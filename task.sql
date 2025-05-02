7433dde5454c3f2d436bb6e195875604dae52e73
Kyrylo Fursov <kirfurs31@gmail.com>
2025-05-02 21:01:10 +0300

    Add transactional insertion

--- task.sql
+++ task.sql
@@ -1,11 +1,23 @@
--- Use our database
 USE ShopDB; 
 
--- Some data should be created outside the transaction (here)
+INSERT INTO Products (Name, Description, Price, WarehouseAmount)
+VALUES ('AwersomeProduct', 'Product Desctiption', 5, 42);
 
--- Start the transaction 
+INSERT INTO Customers (FirstName, LastName, Email, Address)
+VALUES ('John', 'Dou', 'j@dou.ua', 'far, far away');
+
 START TRANSACTION; 
 
--- And some data should be created inside the transaction 
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
+
+SET @order_id = LAST_INSERT_ID();
+
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (@order_id, 1, 1);
+
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE ID = 1;
 
 COMMIT; 
