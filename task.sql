7c92348af2c604759467986f4a7c973ab9ddf4b8
Kyrylo Fursov <kirfurs31@gmail.com>
2025-05-02 21:13:34 +0300

    Enhance SQL script by using variables for dynamic IDs

--- task.sql
+++ task.sql
@@ -1,21 +1,24 @@
-USE ShopDB; 
+USE ShopDB;
 
 INSERT INTO Products (Name, Description, Price, WarehouseAmount)
-VALUES ('AwersomeProduct', 'Product Desctiption', 5, 42);
+VALUES ('AwesomeProduct', 'Product Description', 5, 42);
+SET @product_id = LAST_INSERT_ID();
 
 INSERT INTO Customers (FirstName, LastName, Email, Address)
-VALUES ('John', 'Dou', 'j@dou.ua', 'far, far away');
+VALUES ('John', 'Doe', 'j@doe.ua', 'far, far away');
+SET @customer_id = LAST_INSERT_ID();
 
 INSERT INTO Orders (CustomerID, Date)
-VALUES (1, '2023-01-01');
+VALUES (@customer_id, '2023-01-01');
+SET @order_id = LAST_INSERT_ID();
 
 START TRANSACTION;
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (1, 1, 1);
+VALUES (@order_id, @product_id, 1);
 
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
-WHERE ID = 1;
+WHERE ID = @product_id;
 
 COMMIT;
